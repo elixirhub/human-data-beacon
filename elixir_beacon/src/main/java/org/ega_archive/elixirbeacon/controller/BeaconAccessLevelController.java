074efa27cf24fe2e8a958ace09ff1daf75b930be
Sabela <sabela.delatorre@crg.eu>
2019-04-08 16:29:07 +0200

    Delete line

--- elixir_beacon/src/main/java/org/ega_archive/elixirbeacon/controller/BeaconAccessLevelController.java
+++ elixir_beacon/src/main/java/org/ega_archive/elixirbeacon/controller/BeaconAccessLevelController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/beacon")
 public class BeaconAccessLevelController {
 
-  //  @Autowired
   private BeaconAccessLevelService beaconAccessLevelService;
 
   public BeaconAccessLevelController(BeaconAccessLevelService beaconAccessLevelService) {
