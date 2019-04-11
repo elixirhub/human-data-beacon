a0e77c86305f49d5f2d36086b8f280df669ba649
Sabela <sabela.delatorre@crg.eu>
2019-04-11 16:54:47 +0200

    Create generic method to do the REST call and parse the response

--- elixir_beacon/src/main/java/org/ega_archive/elixirbeacon/utils/ParseResponse.java
+++ elixir_beacon/src/main/java/org/ega_archive/elixirbeacon/utils/ParseResponse.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
-import org.babelomics.csvs.lib.models.DiseaseGroup;
+import org.apache.commons.lang3.StringUtils;
 import org.babelomics.csvs.lib.ws.QueryResponse;
 import org.ega_archive.elixircore.exception.RestRuntimeException;
 import org.ega_archive.elixircore.exception.ServerDownException;
@@ -13,6 +13,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,29 +29,38 @@ public class ParseResponse {
   private ObjectMapper objectMapper;
 
   public <T> QueryResponse<T> parseCsvsResponse(String url, Class<T> clazz) {
+    return (QueryResponse<T>) parseResponse(url, QueryResponse.class, clazz);
+  }
+
+  public <T, U> Object parseResponse(String url, String token, Class<T> clazzT, Class<U> clazzU) {
     ResponseEntity response = null;
     try {
+      MultiValueMap<String, String> header = null;
+      if (StringUtils.isNotBlank(token)) {
+        header = new LinkedMultiValueMap<>();
+        header.add("token", token);
+      }
       response = this.restTemplate
-          .exchange(url, HttpMethod.GET, new HttpEntity(null, null), String.class, new Object[0]);
-    } catch (
-        ResourceAccessException var15) {
-      throw new ServerDownException(var15.getMessage());
+          .exchange(url, HttpMethod.GET, new HttpEntity(null, header), String.class, new Object[0]);
+    } catch (ResourceAccessException ex) {
+      throw new ServerDownException(ex.getMessage());
     }
 
     JavaType type = this.objectMapper.getTypeFactory()
-        .constructParametricType(QueryResponse.class, new Class[]{clazz});
-    QueryResponse basicDTO = null;
+        .constructParametricType(clazzT, new Class[]{clazzU});
+    Object basicDTO = null;
     try {
       basicDTO = this.objectMapper.readValue((String) response.getBody(), type);
-    } catch (
-        IOException var14) {
+    } catch (IOException ex) {
       throw new RestRuntimeException("500",
-          "Exception deserializing object: " + (String) response.getBody() + "\n" + var14
-              .getMessage());
+          "Exception deserializing object: " + response.getBody() + "\n" + ex.getMessage());
     }
     log.debug("response: {}", basicDTO);
-
     return basicDTO;
+  }
+
+  public <T, U> Object parseResponse(String url, Class<T> clazzT, Class<U> clazzU) {
+    return parseResponse(url, null, clazzT, clazzU);
   }
 
 }
