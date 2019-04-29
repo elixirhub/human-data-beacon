352a70b83456896115b7a40c0ecc6a5c2d5b83e2
Sabela <sabela.delatorre@crg.eu>
2019-04-29 17:27:07 +0200

    Rename parameter

--- elixir_beacon/src/main/java/org/ega_archive/elixirbeacon/controller/BeaconAccessLevelController.java
+++ elixir_beacon/src/main/java/org/ega_archive/elixirbeacon/controller/BeaconAccessLevelController.java
@@ -2,11 +2,8 @@ package org.ega_archive.elixirbeacon.controller;
 
 import java.util.List;
 import org.ega_archive.elixirbeacon.dto.AccessLevelResponse;
-import org.ega_archive.elixirbeacon.dto.BeaconOntology;
 import org.ega_archive.elixirbeacon.service.BeaconAccessLevelService;
-import org.ega_archive.elixirbeacon.service.BeaconOntologyService;
 import org.ega_archive.elixircore.constant.ParamName;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,7 +25,7 @@ public class BeaconAccessLevelController {
       @RequestParam(value = ParamName.BEACON_DATASET_IDS, required = false) List<String> datasetStableIds,
       @RequestParam(value = ParamName.BEACON_LEVEL, required = false) String level,
       @RequestParam(value = ParamName.BEACON_INCLUDE_FIELD_DETAILS, required = false, defaultValue = "false") boolean includeFieldDetails,
-      @RequestParam(value = ParamName.BEACON_INCLUDE_DATASET_DETAILS, required = false, defaultValue = "false") boolean includeDatasetDetails
+      @RequestParam(value = ParamName.BEACON_INCLUDE_DATASET_DIFFERENCES, required = false, defaultValue = "false") boolean includeDatasetDetails
   ) {
 
     return beaconAccessLevelService.listAccessLevels(fields, datasetStableIds, level, includeFieldDetails, includeDatasetDetails);
