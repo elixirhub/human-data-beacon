4d2b6a0cd817753b56cb66b07c1222bec3b697a3
sdelatorrep <sabela.delatorrep@gmail.com>
2019-07-16 12:34:31 +0200

    Add handover

---
+++ elixir_beacon/src/main/java/org/ega_archive/elixirbeacon/dto/HandoverType.java
@@ -0,0 +1,16 @@
+package org.ega_archive.elixirbeacon.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HandoverType {
+
+  private String id;
+
+  private String label;
+
+}
