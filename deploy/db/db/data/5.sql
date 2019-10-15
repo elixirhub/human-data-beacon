61f0e983c0660788fca93e2ae6c69551a6e1d9b1
Sabela <sabela.delatorre@crg.eu>
2019-10-15 15:54:58 +0200

    Fix unfinished merge

--- deploy/db/db/data/5.sql
+++ deploy/db/db/data/5.sql
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-TRUNCATE TABLE tmp_sample_table;
-TRUNCATE TABLE tmp_data_sample_table;
-=======
 TRUNCATE TABLE public.tmp_sample_table;
 TRUNCATE TABLE public.tmp_data_sample_table;
-
->>>>>>> v1.1.0_dev
