add4b46ca4d85adc3d2bdef41da4c8b7db7887c5
Sabela <sabela.delatorre@crg.eu>
2019-10-15 15:54:15 +0200

    Fix unfinished merge

--- deploy/db/db/data/3.sql
+++ deploy/db/db/data/3.sql
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-INSERT INTO beacon_dataset_sample_table (dataset_id, sample_id)
-SELECT DISTINCT dat.id AS dataset_id, sam.id AS sample_id
-FROM tmp_sample_table t
-INNER JOIN beacon_sample_table sam ON sam.stable_id=t.sample_stable_id
-INNER JOIN beacon_dataset_table dat ON dat.id=t.dataset_id
-LEFT JOIN beacon_dataset_sample_table dat_sam ON dat_sam.dataset_id=dat.id AND dat_sam.sample_id=sam.id
-WHERE dat_sam.id IS NULL;
-=======
 INSERT INTO public.beacon_dataset_sample_table (dataset_id, sample_id)
 SELECT DISTINCT dat.id AS dataset_id, sam.id AS sample_id
 FROM public.tmp_sample_table t
@@ -14,5 +5,3 @@ INNER JOIN public.beacon_sample_table sam ON sam.stable_id=t.sample_stable_id
 INNER JOIN public.beacon_dataset_table dat ON dat.id=t.dataset_id
 LEFT JOIN public.beacon_dataset_sample_table dat_sam ON dat_sam.dataset_id=dat.id AND dat_sam.sample_id=sam.id
 WHERE dat_sam.id IS NULL;
-
->>>>>>> v1.1.0_dev
